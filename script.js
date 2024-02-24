d5ce3da9d2bb252cb4aa85843928334f2ee865f7
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-02-24 12:09:51 +0100

    HTML structure, CSS calculator layout, basic func
    Created HTML structure for the calculator
    Styled basic look of the calculator
    Created basic calc functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//add operation
+function add(a, b){
+    let result = a + b;
+    console.log(result);
+}
+
+//subtract operation
+function subtract(a, b){
+    let result = a - b;
+    console.log(result);
+}
+
+//multiply operation
+function multiply(a, b){
+    let result = a * b;
+    console.log(result);
+}
+
+//divide operation
+function divide(a, b){
+    let result = a / b;
+    console.log(result);
+}
